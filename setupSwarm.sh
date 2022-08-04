507a82ceb31ad727bb140d4a78736e96ff0e1a33
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-08-04 22:07:53 +0530

    Update setupSwarm.sh

--- setupSwarm.sh
+++ setupSwarm.sh
@@ -2,4 +2,4 @@ sudo docker swarm init
 sudo docker stack deploy -c docker-compose.yml collegemgmtsetup
 sudo docker service ls
 sudo docker service ps collegemgmtsetup_web
-sudo docker ps -a
+sudo docker container ls -q  
